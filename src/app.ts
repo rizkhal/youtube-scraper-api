b6a091c48d46eab8fece38b31482032520c2b442
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2025-03-09 01:48:55 +0700

    fix: ref

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import "dotenv/config";
-import express, { Express, Router } from "express";
 import routes from "./routes";
+import express, { Express } from "express";
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, () => {
+  console.log(`⚡️[app]: Server is running at http://${host}:${port}`);
+});
 
 export default app;
