8bde1eb7f8b0cf9f3383951c7e7f45c40543d9c1
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2025-03-09 22:11:29 +0700

    feat: fix crone jobs

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -1,8 +1,15 @@
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer, { Browser } from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
 export async function createBrowserInstance(): Promise<Browser> {
   return await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox", "--incognito"],
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    args: chromium.args,
   });
+
+  // return await puppeteer.launch({
+  //   headless: true,
+  //   args: ["--no-sandbox", "--disable-setuid-sandbox", "--incognito"],
+  // });
 }
