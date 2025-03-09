9c5a6649375173659da0d5a40840a4f659c7811e
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2025-03-09 22:11:43 +0700

    feat: fix crone jobs

--- dist/lib/browser.js
+++ dist/lib/browser.js
@@ -13,12 +13,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createBrowserInstance = createBrowserInstance;
-const puppeteer_1 = __importDefault(require("puppeteer"));
+const puppeteer_core_1 = __importDefault(require("puppeteer-core"));
+const chrome_aws_lambda_1 = __importDefault(require("chrome-aws-lambda"));
 function createBrowserInstance() {
     return __awaiter(this, void 0, void 0, function* () {
-        return yield puppeteer_1.default.launch({
-            headless: true,
-            args: ["--no-sandbox", "--disable-setuid-sandbox", "--incognito"],
+        return yield puppeteer_core_1.default.launch({
+            executablePath: yield chrome_aws_lambda_1.default.executablePath,
+            headless: chrome_aws_lambda_1.default.headless,
+            args: chrome_aws_lambda_1.default.args,
         });
+        // return await puppeteer.launch({
+        //   headless: true,
+        //   args: ["--no-sandbox", "--disable-setuid-sandbox", "--incognito"],
+        // });
     });
 }
